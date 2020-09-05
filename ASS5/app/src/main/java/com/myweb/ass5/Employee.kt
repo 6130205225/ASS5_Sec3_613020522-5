7a81b4f6820a0e1a146d6ffc39ca64c40e8fa867
613020522-5 <papattaradanai@Kkumail.com>
2020-09-05 20:39:52 +0700

    ASS5_Sec3_613020522-5
    ASS5_Sec3_613020522-5

---
+++ ASS5/app/src/main/java/com/myweb/ass5/Employee.kt
@@ -0,0 +1,10 @@
+package com.myweb.ass5
+
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class Employee(val name:String, val gender:String, val email:String, val salary:Int):Parcelable{
+
+}
